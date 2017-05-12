786511519389c8504462a5de70f1a12c567ab17b
士豪 <@PETER-PC>
2017-05-12 13:31:17 +0800

    加入專案檔案。

---
+++ Week1-Homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week1_Homework.MvcApplication" Language="C#" %>
---
+++ Week1-Homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week1_Homework.Startup))]
+namespace Week1_Homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
